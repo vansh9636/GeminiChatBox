73b6afaf39ddef912032605da940cc2b3851f030
vansh9636 <kumawatvansh365@gmail.com>
2025-12-10 20:27:08 +0530

    Updated feature / fixed bug / new changes

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,6 +1,6 @@
 // this file is for configuring Gemini API access from Google Generative AI
-const ourapi = "AIzaSyA5f23FBWDVG412IbAuK5SR8LlKCiHAh8s"// your api key here
-// AIzaSyA5f23FBWDVG412IbAuK5SR8LlKCiHAh8s
+const ourapi = ""// your api key here
+
 
 import {
   GoogleGenerativeAI,
